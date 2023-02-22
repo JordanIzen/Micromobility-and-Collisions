b0b8c8d7542525a33217dcf9f2adea38f6c76ec9
JordanIzen <101665679+JordanIzen@users.noreply.github.com>
2023-02-21 20:48:32 -0500

    Update Clean Data.R

--- Clean Data.R
+++ Clean Data.R
@@ -23,9 +23,37 @@ clean_dat$`CRASH DATE` <- mdy(clean_dat$`CRASH DATE`)
 bike_accident <- clean_dat %>% 
   filter(`NUMBER OF CYCLIST INJURED` >0 | `NUMBER OF CYCLIST KILLED` >0)
 
+
+# Investigate how taxi was reported
+unique(unlist(clean_dat$`VEHICLE TYPE CODE 1`))
+
 # Create data set where taxis are involved
+taxi_types <- c('Taxi', 'TAXI', 'YELLOW TAXI', 'yellow taxi', 'yellow cab', 'CAB', 'cab')
 taxi_accident <- clean_dat %>% 
-  filter(`VEHICLE TYPE CODE 1` == 'Taxi' | `VEHICLE TYPE CODE 2` == 'Taxi' | `VEHICLE TYPE CODE 3` == 'Taxi' | `VEHICLE TYPE CODE 4` == 'Taxi' | `VEHICLE TYPE CODE 5` == 'Taxi' | `VEHICLE TYPE CODE 1` == 'TAXI' | `VEHICLE TYPE CODE 2` == 'TAXI' | `VEHICLE TYPE CODE 3` == 'TAXI' | `VEHICLE TYPE CODE 4` == 'TAXI' | `VEHICLE TYPE CODE 5` == 'TAXI')
+  filter(`VEHICLE TYPE CODE 1` %in% taxi_types | `VEHICLE TYPE CODE 2` %in% taxi_types | `VEHICLE TYPE CODE 3` %in% taxi_types | `VEHICLE TYPE CODE 4` %in% taxi_types | `VEHICLE TYPE CODE 5` %in% taxi_types)
+
+
+# Plot data to get feel for trends
+## Agg data by month-year
+taxi_accident_month <- taxi_accident %>% 
+  mutate(month = month(`CRASH DATE`),
+         year = year(ymd(`CRASH DATE`))) %>% 
+  group_by(month,year) %>% 
+  summarise(count = n()) %>% 
+  relocate(year,month) %>% 
+  unite('year_month', year:month, sep = '-', remove = F) %>% 
+  mutate(year_month = ym(year_month))
+  
+
+ggplot(taxi_accident_month, aes(x=year_month, y = count)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal()
+
+
+
+
+
 
 
 
